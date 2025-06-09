d0cb95db7397cbc662782cb8d781236ad8ea3d95
JonasGTQ <jonasgtq@gmail.com>
2025-06-09 17:31:11 -0300

    imgs funfando

--- PrjMobileTCC/app/src/main/java/com/example/prjmobiletcc/DetalhesFragment.java
+++ PrjMobileTCC/app/src/main/java/com/example/prjmobiletcc/DetalhesFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +39,7 @@ public class DetalhesFragment extends Fragment {
 
     Valores vlrs = new Valores();
     Carrinho carrinho = new Carrinho();
+    LinearLayout lytimg;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ver = inflater.inflate(R.layout.fragment_detalhes, container,false);
@@ -58,6 +61,7 @@ public class DetalhesFragment extends Fragment {
         txtvendido = ver.findViewById(R.id.txtvQuantvendido);
         txtestoque = ver.findViewById(R.id.txtvQuantdisponivel);
         btnadcionanocar = ver.findViewById(R.id.btnAdicionacarrinho);
+        lytimg = ver.findViewById(R.id.lytCntnrbcimg);
         grdlogin = new Guardalogin(requireContext());
 
 
@@ -88,7 +92,17 @@ public class DetalhesFragment extends Fragment {
         try{
             Cnxbd bdcnx = new Cnxbd();
             bdcnx.entBanco(requireContext());
-            bdcnx.RS = bdcnx.stmt.executeQuery("SELECT p.*, COALESCE(COUNT(pp.Id_Pedido), 0) AS Quantidade_Total_Produto_Pedido " +
+            bdcnx.RS = bdcnx.stmt.executeQuery("SELECT " +
+                    "p.Id_Produto," +
+                    "p.Nome_Produto," +
+                    "p.Descricao_Produto," +
+                    "p.Valor_Produto," +
+                    "p.Desconto_Produto," +
+                    "p.Tamanho_Produto," +
+                    "p.Quantidade_Produto," +
+                    "p.Tecido_Produto," +
+                    "p.Cor_Produto," +
+                    " COALESCE(COUNT(pp.Id_Pedido), 0) AS Quantidade_Total_Produto_Pedido " +
                     "FROM Produto p " +
                     "LEFT JOIN Produto_Pedido pp ON pp.Id_Produto = p.Id_Produto " +
                     "WHERE p.Id_Produto = "+vlrs.idproduto+" " +
@@ -107,7 +121,6 @@ public class DetalhesFragment extends Fragment {
                         cor_Produto,
                         verda_Produto;
                 nome_Produto = bdcnx.RS.getString("Nome_Produto");
-
                 descricao_Produto = bdcnx.RS.getString("Descricao_Produto");
                 valor_Produto = bdcnx.RS.getString("Valor_Produto");
                 desconto_Produto = bdcnx.RS.getString("Desconto_Produto");
@@ -135,29 +148,81 @@ public class DetalhesFragment extends Fragment {
                 txtdesconto.setText(desconto_Produto+"%");
                 txtvendido.setText(verda_Produto);
                 txtestoque.setText(quantidade_Produto);
-                try {
-                    bdcnx.entBanco(requireContext());
-                    bdcnx.RS = bdcnx.stmt.executeQuery("SELECT Url_ImgProduto FROM Imagem_Produto WHERE Id_Produto = " + vlrs.idproduto + " ORDER BY Ordem_ImgProduto ASC");
 
-                    int index = 0;
-                    ImageView[] imgViews = {imgvslide, imgvcx1, imgvcx2, imgvcx3, imgvcx4};
-
-                    while (bdcnx.RS.next()) {
-                        String imgurl = bdcnx.RS.getString("Url_ImgProduto");
-                        if (index < imgViews.length) {
-                            new CarregaImagem(imgViews[index]).execute(bdcnx.urlimgsrv + imgurl);
-                            index++;
-                        }
-                    }
-                } catch (SQLException ex) {
-                    System.out.println(ex);
-                }
 
                 vlrs.descricaoProduto = descricao_Produto;
+                carregaimg();
             }
         }catch (SQLException ex){
             System.out.println(ex);
         }
     }
+    private void carregaimg(){
+        lytimg.removeAllViews();
+        try {
+            Cnxbd bdcnx = new Cnxbd();
+            bdcnx.entBanco(requireContext());
+            bdcnx.RS = bdcnx.stmt.executeQuery("SELECT Url_ImgProduto FROM Imagem_Produto WHERE Id_Produto = " + vlrs.idproduto + " ORDER BY Ordem_ImgProduto ASC");
+            while (bdcnx.RS.next()) {
+                String imgurl = bdcnx.RS.getString("Url_ImgProduto");
+                    criacntnr(bdcnx.urlimgsrv + imgurl);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        try {
+            Cnxbd bdcnx = new Cnxbd();
+            bdcnx.entBanco(requireContext());
+            bdcnx.RS = bdcnx.stmt.executeQuery("SELECT Url_ImgProduto FROM Imagem_Produto WHERE Id_Produto = " + vlrs.idproduto + " ORDER BY Ordem_ImgProduto ASC");
+
+
+            if (bdcnx.RS.next()) {
+                String endeimg = bdcnx.RS.getString("Url_ImgProduto");
+                new CarregaImagem(imgvslide).execute(bdcnx.urlimgsrv+endeimg);
+            }
+        }catch (SQLException ex){
+            System.out.println(ex);
+        }
+    }
+    private void criacntnr(String imgende){
+        // Criando um CardView
+        CardView cardView = new CardView(requireContext());
+        cardView.setLayoutParams(new LinearLayout.LayoutParams(
+                dpToPx(75), // Largura convertida para pixels
+                dpToPx(75)  // Altura convertida para pixels
+        ));
+        cardView.setCardElevation(4);
+        cardView.setRadius(dpToPx(15));
+
+        // Definindo margens
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) cardView.getLayoutParams();
+                params.setMargins(dpToPx(5), 0, dpToPx(5), 0);
+                params.weight = 1;
+                cardView.setLayoutParams(params);
+
+        // Criando ImageView dentro do CardView
+                ImageView imageView = new ImageView(requireContext());
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                ));
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                new CarregaImagem(imageView).execute(imgende);
+
+        // Adicionando ImageView ao CardView
+                cardView.addView(imageView);
+                cardView.setOnClickListener(view -> {
+                    new CarregaImagem(imgvslide).execute(imgende);
+                });
+
+                lytimg.addView(cardView);
+
+    }
+    private int dpToPx(int dp) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
 
 }
